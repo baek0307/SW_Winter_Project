435e09e9905f87b7eeb26030128cc6d3d6a58f44
baek0307 <baek0307@users.noreply.github.com>
2016-03-02 18:28:23 +0900

    Added files via upload
    아두이노 프로그램 특징상 한글을 사용해 주석을 달경우 오류가 발생 
    
    지금 올린 프로그램은 아두이노의 블루투스 모듈이 스마트폰과 제대로 연동이 되는지를 확인하기 위한 간단한코드 1이 입력되면 전구에 불이 들어오고 2가 입력되면 전구의 불이꺼지는 그밖의 단어가 입력될경우  "Send 1 or 2"라는 문구가 뜬다

---
+++ _20160302_0623PM.ino
@@ -0,0 +1,29 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial BT(10,11);
+  void setup(){
+  
+    pinMode(13,OUTPUT);//전전원불들엉온늕확읺항슴맡틒폰블뤁틋몯젣댈작동한늕확
+    BT.begin(9600);
+    BT.println("Hello My name is BAEK");
+  }
+  
+  char a;
+  void loop(){
+    if(BT.available()){
+      a = (BT.read());
+      if(a=='1'){
+        digitalWrite(13,HIGH);
+        BT.println("LED on");
+      }
+      
+      if(a=='2'){
+        digitalWrite(13,LOW);
+        BT.println("LED off");
+      }
+      if(a=='?'){
+        BT.println("Send '1' or '2'");
+      } 
+      
+    }
+  }
